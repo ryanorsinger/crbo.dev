612183634fbe85335a06f350a58aa0bdc3bc9523
IndianAg0711 <joseph.villafranca@gmail.com>
2014-04-16 11:15:02 -0500

    added auth filter to base control, styles to master

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -11,6 +11,8 @@ class BaseController extends Controller {
 	public function __construct ()
 	{
 		$this->beforeFilter('csrf', array('on' => array('post', 'delete', 'put')));
+
+		$this->beforeFilter('auth', ['except' => ['showLogin', 'doLogin']]);
 	}
 
 	protected function setupLayout()
