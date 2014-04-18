90115b63a44ba5c50ef5d66b326b699bb9106b9e
Izzy Rodriguez <izzy@210geeks.com>
2014-04-18 17:13:05 -0500

    Updated admin / home views

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -7,13 +7,15 @@
 
 @section('style')
 <style type="text/css">
-#buyback {
-	font-size: 13em;
+#crbo {
+	font-size: 1.65em;
 	color: rgb(85,85,85);
+	text-align: center;
 }
-#refurb {
-	font-size: 13em;
-	color: rgb(85,85,85);
+
+#buyback {
+	margin-top: 19px;
+
 }
 
 </style>
@@ -24,19 +26,16 @@
 <br>
 <br>
 <br>
-<div class="container-fluid">
+<div class="container-fluid"; id="crbo">
 	<div class="row">
-		<div class="col-md-3 col-md-offset-2">
-			<a href="{{{ action('DevicesController@create') }}}"><img src="/img/desktop.png"></a>
-			<p>Buy / Sell</p>
+		<div class="col-md-3 col-md-offset-3" id="buyback">
+			<a href="{{{ action('DevicesController@create') }}}"><img src="/img/desktop.png"><br>Buy / Sell</a>
+			
 		</div>
-		<div class="col-md-3">
-			<a href="{{{ action('DevicesController@create') }}}"><img src="/img/recycling.png"></a>
-			<p>Recycle</p>
-		</div>
-		<div class="col-md-3">
-			<a href="{{{ action('DevicesController@index') }}}"><img src="/img/refurb.png"></a>
-			<p>Refurbish</p>
+		
+		<div class="col-md-3" id="refurbish">
+			<a href="{{{ action('DevicesController@index') }}}"><img src="/img/refurb.png"><br>Refurbish</a>
+			
 		</div>
 	</div>
 </div>
